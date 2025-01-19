3b00305196144f4095b9725e97ba7f9bbada58e3
junya3 <obarajunya.0728@gmail.com>
2025-01-20 01:12:02 +0900

    RankingSecコンポーネントでのランキングデータの取得元を変更し、ranking.jsを追加

---
+++ src/data/ranking.js
@@ -0,0 +1,134 @@
+import image01 from '/src/assets/image/contents/contents-image1.png';
+import image02 from '/src/assets/image/contents/contents-image2.png';
+import image03 from '/src/assets/image/contents/contents-image3.png';
+import image04 from '/src/assets/image/contents/contents-image4.png';
+import image05 from '/src/assets/image/contents/contents-image5.png';
+import image06 from '/src/assets/image/contents/contents-image6.png';
+import image07 from '/src/assets/image/contents/contents-image7.png';
+import image08 from '/src/assets/image/contents/contents-image8.png';
+import image09 from '/src/assets/image/contents/contents-image9.png';
+import image10 from '/src/assets/image/contents/contents-image10.png';
+import image11 from '/src/assets/image/contents/contents-image11.png';
+import image12 from '/src/assets/image/contents/contents-image12.png';
+import image13 from '/src/assets/image/contents/contents-image13.png';
+import image14 from '/src/assets/image/contents/contents-image14.png';
+import image15 from '/src/assets/image/contents/contents-image15.png';
+import image16 from '/src/assets/image/contents/contents-image16.png';
+import image17 from '/src/assets/image/contents/contents-image17.png';
+import image18 from '/src/assets/image/contents/contents-image18.png';
+
+export default {
+  content: [
+    {
+      title: 'タイトル1',
+      image: image01,
+      alt: 'タイトル1の様子',
+      description: '駄菓子',
+    },
+    {
+      title: 'タイトル2',
+      image: image02,
+      alt: 'タイトル2の様子',
+      description: 'たこ焼き',
+      ranking: 2,
+    },
+    {
+      title: 'タイトル3',
+      image: image03,
+      alt: 'タイトル3の様子',
+      description: 'クレープ',
+    },
+    {
+      title: 'タイトル4',
+      image: image04,
+      alt: 'タイトル4の様子',
+      description: 'フランクフルト',
+    },
+    {
+      title: 'タイトル5',
+      image: image05,
+      alt: 'タイトル5の様子',
+      description: 'かき氷',
+    },
+    {
+      title: 'タイトル6',
+      image: image06,
+      alt: 'タイトル6の様子',
+      description: '綿菓子',
+    },
+    {
+      title: 'タイトル7',
+      image: image07,
+      alt: 'タイトル7の様子',
+      description: 'チョコバナナ',
+    },
+    {
+      title: 'タイトル8',
+      image: image08,
+      alt: 'タイトル8の様子',
+      description: 'お好み焼き',
+    },
+    {
+      title: 'タイトル9',
+      image: image09,
+      alt: 'タイトル9の様子',
+      description: '唐揚げ',
+    },
+    {
+      title: 'タイトル10',
+      image: image10,
+      alt: 'タイトル10の様子',
+      description: '焼き鳥',
+    },
+    {
+      title: 'タイトル11',
+      image: image11,
+      alt: 'タイトル11の様子',
+      description: 'ポップコーン',
+    },
+    {
+      title: 'タイトル12',
+      image: image12,
+      alt: 'タイトル12の様子',
+      description: 'ホットドッグ',
+      ranking: 1,
+    },
+    {
+      title: 'タイトル13',
+      image: image13,
+      alt: 'タイトル13の様子',
+      description: 'インドカレー',
+    },
+    {
+      title: 'タイトル14',
+      image: image14,
+      alt: 'タイトル14の様子',
+      description: 'ラーメン',
+    },
+    {
+      title: 'タイトル15',
+      image: image15,
+      alt: 'タイトル15の様子',
+      description: 'スイーツパフェ',
+    },
+    {
+      title: 'タイトル16',
+      image: image16,
+      alt: 'タイトル16の様子',
+      description: '餃子',
+    },
+    {
+      title: 'タイトル17',
+      image: image17,
+      alt: 'タイトル17の様子',
+      description: '沖縄そば',
+    },
+    {
+      title: 'タイトル18',
+      image: image18,
+      alt: 'タイトル18の様子',
+      description: 'ソフトクリーム',
+      ranking: 3,
+    },
+  ],
+};
